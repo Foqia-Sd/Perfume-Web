a3f7b44f95fefcc595f8d0c9e027e1a782ad3408
Foqia-Sd <160354822+Foqia-Sd@users.noreply.github.com>
2025-07-04 15:36:45 +0500

    Error Resolve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import Discount from "@/components/Discount";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
-import Image from "next/image";
-
 
 export default function Home() {
   return (
